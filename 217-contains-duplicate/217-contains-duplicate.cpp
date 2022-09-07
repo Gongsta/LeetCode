4a7a5f75edc81dafc83c1628ff8bf07098ee817e
Steven Gong <gong.steven@hotmail.com>
2022-09-07 19:25:49 -0400

    Time: 237 ms (6.75%), Space: 52.2 MB (35.11%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s;
+        for (auto num: nums) {
+            if (s.count(num)) {
+                return true;
+            }
+            s.insert(num);
+        }
+        return false;
+            
+           
+        
+    }
+};
