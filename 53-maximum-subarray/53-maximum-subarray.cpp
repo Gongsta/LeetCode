4d6a68eb9dc426d10fc41dee32abab321f801f02
Steven Gong <gong.steven@hotmail.com>
2022-09-15 20:12:18 -0400

    Time: 377 ms (5.04%), Space: 67.7 MB (53.70%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = nums[0];
+        int i = 0;
+        int l = 0;
+        int curr_sum = 0;
+        while (i + l < nums.size()) {
+            curr_sum += nums[i+l];
+            ans = max(ans, curr_sum);
+            if (curr_sum <= 0) {
+                curr_sum = 0;
+                l = l+i + 1;
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+           
+        
+        return ans;
+        
+    }
+};
