ace4de5dc7f989532f4d4076517d0594da11532d
Steven Gong <gong.steven@hotmail.com>
2023-06-02 15:53:17 -0400

    Time: 92 ms (10.98%), Space: 15.1 MB (32.67%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int dp[101][101][201];
+    bool solve(string& s1, string& s2, string& s3, int s1_start, int s2_start, int s3_start) {
+        if (dp[s1_start][s2_start][s3_start] != -1) {
+            return dp[s1_start][s2_start][s3_start];
+            
+        }
+        bool works = false;
+        if (s1_start == s1.size() && s2_start == s2.size() && s3_start == s3.size()) {
+            return dp[s1_start][s2_start][s3_start] = true;
+        }
+        if (s3_start == s3.size()) {
+            return dp[s1_start][s2_start][s3_start] = false;
+        }
+        bool found = false;
+        if (s1_start != s1.size()) {
+            if (s1[s1_start] == s3[s3_start]) {
+                works = works || solve(s1, s2, s3, s1_start+1, s2_start, s3_start+1);
+                found = true;
+            }
+        }
+        if (s2_start != s2.size()) {
+            if (s2[s2_start] == s3[s3_start]) {
+                works = works || solve(s1, s2, s3, s1_start, s2_start+1, s3_start+1);
+                found = true;
+            }
+        }
+        if (!found) {
+            return dp[s1_start][s2_start][s3_start] = false;
+        }
+        
+        return dp[s1_start][s2_start][s3_start] = works;
+            
+        
+    }
+    
+    bool isInterleave(string s1, string s2, string s3) {
+        memset(dp, -1, sizeof(dp));
+        return solve(s1, s2, s3, 0, 0, 0);
+    }
+};
+
