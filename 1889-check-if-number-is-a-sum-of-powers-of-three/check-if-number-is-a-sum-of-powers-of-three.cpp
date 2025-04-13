a3ff81e66974c6d6151fcf95b58c588d98f5285d
Steven Gong <gong.steven@hotmail.com>
2025-04-12 20:01:28 -0700

    Time: 43 ms (6.37%) | Memory: 8 MB (12.82%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool search(int n, int pow) {
+        if (n == 0) return true;
+        if (n < 0) return false;
+        bool works = false;
+        for (int i = pow;i<100;i++) {
+            if (n - std::pow(3, i) < 0) break;
+            works |= search (n - std::pow(3, i), i + 1);
+        }
+        return works;
+    }
+    bool checkPowersOfThree(int n) {
+        if (n == 1) return true;
+        return search(n, 0);
+        
+    }
+};
