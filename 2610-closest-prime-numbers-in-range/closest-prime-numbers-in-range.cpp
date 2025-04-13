21cfc39a255df9fe2c509581da4f2fae8e015abd
Steven Gong <gong.steven@hotmail.com>
2025-04-12 20:13:56 -0700

    Time: 307 ms (34.12%) | Memory: 80.5 MB (17.79%) - LeetSync

---
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<long long> generate_primes(int max){
+        vector<bool> vec(max+1, true);
+        vec[0] = false;
+        vec[1] = false;
+        vector<long long> primes;
+        for (long long i = 0;i<max+ 1;i++) {
+            if (!vec[i]) continue;
+            primes.push_back(i);
+            for (long long j = i*i;j<max+1;j+=i) {
+                vec[j] = false;
+            }
+        }
+        return primes;
+    }
+
+    vector<int> closestPrimes(int left, int right) {
+        vector<int> ans;
+        vector<long long> primes = generate_primes(right);
+        if (primes.size() <= 1) return {-1,-1};
+        for (long long i = 0;i<primes.size() - 1;i++) {
+            if (primes[i] < left) continue;
+            if (ans.empty()) {
+                ans.push_back(primes[i]);
+                ans.push_back(primes[i+1]);
+            } else {
+                int diff = ans[1] - ans[0];
+                if (primes[i+1] - primes[i] < diff) {
+                    ans[0] = primes[i];
+                    ans[1] = primes[i + 1];
+                }
+            }
+        }
+        if (ans.empty()) {
+            return {-1,-1};
+        }
+        return ans;
+        
+    }
+};
