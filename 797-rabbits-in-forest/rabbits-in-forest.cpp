198655150bdc0089862ad53afe4a9be7cfcba63e
Steven Gong <gong.steven@hotmail.com>
2025-04-20 14:21:28 -0700

    Time: 2 ms (28.80%) | Memory: 12.4 MB (10.60%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        int ans = 0;
+        map<int, int> m;
+        for (auto answer: answers) {
+            m[answer]++;
+        }
+        for (auto x: m) {
+            if (x.first >= x.second) {
+                ans += x.first + 1;
+            } else  {
+                int divs = x.second / (x.first + 1);
+                if (divs * (x.first + 1) == x.second) {
+                    ans += x.second;
+                } else {
+                    ans += (divs + 1) * (x.first + 1);
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
