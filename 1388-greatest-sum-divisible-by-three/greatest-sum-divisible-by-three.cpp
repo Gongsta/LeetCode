fb439c29a18a92c710032adfea2b19364c09f983
Steven Gong <gong.steven@hotmail.com>
2025-11-23 08:28:13 -0500

    Time: 8 ms (70.91%) | Memory: 28.8 MB (84.90%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        int mod_0 = 0;
-        int mod_1 = -1e9;
-        int mod_2 = -1e9;
+        /*
+        dp[(r + num) % 3] = max(dp[(r + num) % 3], dp[r] + num));
+        return dp 0,
+        */
+        vector<int> dp = {0, INT_MIN, INT_MIN};
+        vector<int> prev;
         for (int num: nums) {
-            int prev_mod_0 = mod_0;
-            int prev_mod_1 = mod_1;
-            int prev_mod_2 = mod_2;
-            if (num % 3 == 0) {
-                mod_0 = max(max(prev_mod_0, prev_mod_0 + num), num);
-                mod_1 = max(prev_mod_1, prev_mod_1 + num);
-                mod_2 = max(prev_mod_2, prev_mod_2 + num);
-            } else if (num % 3 == 1) {
-                mod_0 = max(prev_mod_0, prev_mod_2 + num);
-                mod_1 = max(max(prev_mod_1, prev_mod_0 + num), num);
-                mod_2 = max(prev_mod_2, prev_mod_1 + num);
-            } else {
-                mod_0 = max(prev_mod_0, prev_mod_1 + num);
-                mod_1 = max(prev_mod_1, prev_mod_2 + num);
-                mod_2 = max(max(prev_mod_2, prev_mod_0 + num), num);
+            prev = dp;
+            for (int r = 0;r<3;r++) {
+                dp[(r+num) % 3] = max(dp[(r+num) % 3], prev[r] + num);
             }
         }
-        return mod_0;
+        return dp[0];
     }
 };
