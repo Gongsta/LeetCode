abc33c6f2a0b8f0ee3e66c65742881dd2c4b389f
Steven Gong <gong.steven@hotmail.com>
2025-11-23 22:28:43 -0500

    Time: 0 ms (100.00%) | Memory: 9.1 MB (71.97%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> digitToLetters(char digit_char) {
+        int digit = digit_char - '0';
+        if (digit <= 6) {
+            char a = 'a' + (digit-2)* 3;
+            char b = 'a' + (digit-2)* 3 + 1;
+            char c = 'a' + (digit-2)* 3 + 2;
+            return {string{a}, string{b}, string{c}};
+
+        } else if (digit == 7) {
+            return {"p", "q", "r", "s"};
+        } else if (digit == 8) {
+            return {"t", "u", "v"};
+        } else {
+            return {"w", "x", "y", "z"};
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.size() == 1) return digitToLetters(digits[0]);
+        vector<string> combinations = letterCombinations(digits.substr(1));
+        vector<string> base_letters = digitToLetters(digits[0]);
+        vector<string> all_combinations;
+        for (const string& letter: base_letters) {
+            cout << letter << endl;
+            for (const string& word: combinations) {
+                all_combinations.push_back(letter + word);
+            }
+        }
+
+        return all_combinations;
+        
+    }
+};
