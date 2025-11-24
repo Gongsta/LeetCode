db26aa2b798e69a144d3e7432ee3dc529f46105b
Steven Gong <gong.steven@hotmail.com>
2025-11-23 19:38:20 -0500

    Time: 161 ms (5.83%) | Memory: 77.9 MB (9.24%) - LeetSync

---
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int ans = 0;
+        map<int, int> cnt;
+        for (int num: nums) {
+            if (cnt[k - num] > 0) {
+                ans++;
+                cnt[k-num]--;
+            } else {
+                cnt[num]++;
+            }
+        }
+        return ans;
+    }
+};
