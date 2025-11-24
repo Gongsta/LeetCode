8efc7e547c2da897126828b29b5f0bce809e6931
Steven Gong <gong.steven@hotmail.com>
2025-11-24 13:49:20 -0500

    Time: 16 ms (69.39%) | Memory: 27.6 MB (87.02%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     string removeStars(string s) {
-        int counter = 0;
+        int skip = 0;
         string ans;
         for (int i = s.length() - 1;i>=0;i--) {
             if (s[i] == '*') {
-                counter++;
-            } else {
-                if (counter == 0) {
-                    ans += s[i];
-                } else {
-                    counter--;
-                }
+                skip++;
+            } else if (skip > 0) {
+                skip--;
+            } else { // No letters we need to skip, add it to answer
+                ans += s[i];
             }
         }
         reverse(ans.begin(), ans.end());
