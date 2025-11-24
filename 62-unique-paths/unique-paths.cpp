b5a0d64462956030e9c496d2aed0bf94c1b89bca
Steven Gong <gong.steven@hotmail.com>
2025-11-23 20:07:52 -0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (92.25%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -2,11 +2,19 @@ class Solution {
     map<pair<int, int>, int> memo;
 public:
     int uniquePaths(int m, int n) {
-        if (m == 1 || n == 1) return 1;
-        if (memo.count({m, n})) return memo[{m,n}];
-        int ans = 0;
-        ans += uniquePaths(m - 1, n);
-        ans += uniquePaths(m, n - 1);
-        return memo[{m,n}] = ans;
+        /*
+        dp[m][n] = dp[m][n-1] + dp[m-1][n]
+        */
+        int dp[m+1][n+1];
+        for (int i = 1;i<=m;i++) {
+            for (int j = 1;j<=n;j++) {
+                if (i == 1 || j == 1) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j-1];;
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
