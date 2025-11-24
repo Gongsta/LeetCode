4c70ed644699764a299c9f7bbf3df5c5beeb70fe
Steven Gong <gong.steven@hotmail.com>
2025-11-24 12:42:36 -0500

    Time: 11 ms (70.89%) | Memory: 13.4 MB (81.27%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
@@ -5,28 +5,28 @@ public:
         int all_visited_bitmask = (1 << n) - 1;
         int ans = INT_MAX;
 
+
+        vector<vector<bool>> visited(n, vector<bool>(1 << n, false));
+        queue<tuple<int, int, int>> q;
         for (int start = 0;start < n; start++) {
-            vector<vector<bool>> visited(n, vector<bool>(1 << n, false));
-            queue<tuple<int, int, int>> q;
             q.push({start, (1 << start), 0});
-
-            // Run BFS
-            while (!q.empty()) {
-                auto [node, bitmask, dist] = q.front(); q.pop();
-                if (bitmask == all_visited_bitmask) {
-                    ans = min(ans, dist);
-                    break;
-                }
-
-                for (int neigh: graph[node]) {
-                    int new_bitmask = bitmask | (1 << neigh);
-                    if (visited[neigh][new_bitmask]) continue;
-                    visited[neigh][new_bitmask] = true;
-                    q.push({neigh, new_bitmask, dist + 1});
-                }
+        }
+        // Run BFS
+        while (!q.empty()) {
+            auto [node, bitmask, dist] = q.front(); q.pop();
+            if (bitmask == all_visited_bitmask) {
+                ans = min(ans, dist);
+                break;
             }
 
+            for (int neigh: graph[node]) {
+                int new_bitmask = bitmask | (1 << neigh);
+                if (visited[neigh][new_bitmask]) continue;
+                visited[neigh][new_bitmask] = true;
+                q.push({neigh, new_bitmask, dist + 1});
+            }
         }
+
         return ans;
         
     }
