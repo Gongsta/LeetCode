16f8355b327d9c0ab7b5ee5663d1dd9a0e5d7135
Steven Gong <gong.steven@hotmail.com>
2025-11-23 19:33:19 -0500

    Time: 0 ms (100.00%) | Memory: 62.9 MB (47.95%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,19 +4,15 @@ public:
         int ans = 0;
         int l = 0;
         int r = height.size() - 1;
-        while (l <= r) {
-            int l_val = height[l];
-            int r_val = height[r];
-            int area = min(l_val, r_val) * (r - l);
-            ans = max(ans, area);
-            if (l_val < r_val) {
-               l++;
+
+        while (l < r) {
+            ans = max(ans, min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
             } else {
                 r--;
             }
         }
-
         return ans;
-        
     }
 };
