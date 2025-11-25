6f5752f80062839e26da2a1faa45f2969687bb39
Steven Gong <gong.steven@hotmail.com>
2025-11-24 19:22:26 -0500

    Time: 0 ms (100.00%) | Memory: 21.7 MB (77.85%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -3,29 +3,19 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> final_asteroids;
         for (int asteroid: asteroids) {
-            if (final_asteroids.empty() || asteroid > 0) {
-                final_asteroids.push_back(asteroid);
-            } else { // Asteroid pointing left (negative)
-                // Resolve ongoing collisions or add valeus to stack.
-                while (!final_asteroids.empty()) {
-                    if (final_asteroids.back() < 0) {
-                        final_asteroids.push_back(asteroid);
-                        break;
-                    } else if (final_asteroids.back() > abs(asteroid)) { // asteroid gets destroyed
-                        break;
-                    } else if (final_asteroids.back() < abs(asteroid)) { // Keep destroying
-                        final_asteroids.pop_back();
-                        // Check if we destroyed the last one
-                        if (final_asteroids.empty()) {
-                            final_asteroids.push_back(asteroid);
-                            break;
-                        }
-                    } else { // same size, both get destroyed and terminate
-                        final_asteroids.pop_back();
-                        break;
-                    }
+            bool alive = true;
+            // While collision is happening
+            while (!final_asteroids.empty() && final_asteroids.back() > 0 && alive && asteroid < 0) {
+                if (final_asteroids.back() > -asteroid) { // asteroid gets destroyed
+                    alive = false;
+                } else if (final_asteroids.back() < -asteroid) { // Keep destroying
+                    final_asteroids.pop_back();
+                } else { // same size, both get destroyed and terminate
+                    final_asteroids.pop_back();
+                    alive = false;
                 }
             }
+            if (alive) final_asteroids.push_back(asteroid);
         }
         return final_asteroids;
     }
