51512ede69282700f9e2e985c525b28b03a66962
Steven Gong <gong.steven@hotmail.com>
2025-11-26 17:35:38 -0500

    Time: 23 ms (33.29%) | Memory: 25.9 MB (56.30%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        /*
+        Keep track of kinds of anagrams we've seem through hash map
+        
+        For each entry of hash map, retrieve the values
+        */
+        // Populate hash map
+        unordered_map<string, vector<string>> anagram_to_idxs;
+        for (int i = 0;i<strs.size();i++) {
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            anagram_to_idxs[s].push_back(strs[i]);
+        }
+
+        // Compute answer
+        vector<vector<string>> ans;
+        for (auto [anagram, strings]: anagram_to_idxs) {
+            // Store strings
+            ans.push_back(strings);
+        }
+        return ans;
+        
+    }
+};
