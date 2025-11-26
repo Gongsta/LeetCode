8d1177fd367160fa113fa6172c19c285a810b575
Steven Gong <gong.steven@hotmail.com>
2025-11-26 17:46:23 -0500

    Time: 12 ms (92.55%) | Memory: 60 MB (99.18%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -5,17 +5,17 @@ public:
         
         // Sort and keep a running counter. Ignore duplicates
         sort(nums.begin(), nums.end());
+
         int ans = 1;
-        int curr = nums[0];
         int curr_seq = 1;
+
         for (int i = 1;i<nums.size();i++) {
-            if (nums[i] == curr) continue;
-            if (nums[i] == curr + 1) {
+            if (nums[i] == nums[i-1])  {
+                continue;
+            } else if (nums[i] == nums[i-1] + 1) {
                 curr_seq++;
-                curr = nums[i];
-                ans =max (ans, curr_seq);
-            } else {
-                curr = nums[i];
+                ans = max(ans, curr_seq);
+            } else { // reset
                 curr_seq = 1;
             }
         }
