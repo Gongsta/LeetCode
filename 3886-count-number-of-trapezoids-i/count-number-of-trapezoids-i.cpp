4f19bed0e0c1d5466e8429e1254e89022bd01833
Steven Gong <gong.steven@hotmail.com>
2025-12-03 16:38:43 -0500

    Time: 119 ms (27.97%) | Memory: 193.8 MB (58.83%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -3,8 +3,8 @@ public:
     int countTrapezoids(vector<vector<int>>& points) {
         /*
         1. Count the number of points for each "y"
-        2. Compute n choose 2 for each y, and compute a running suffix
-        3. Iterate over each y, and multiple and remaining number of point computations
+        2. Compute n choose 2 for each y, and add to total number of points.
+        3. Iterate over each y, and multiple and remaining number of point computations, remove suffix
         */
         map<int, long long> y_cnt;
         for (vector<int>& point: points) {
@@ -13,16 +13,14 @@ public:
         int MOD = (int)1e9 + 7;
         long long total_line_combos = 0;
         for (auto& x: y_cnt) {
-            total_line_combos += x.second * (x.second - 1) / 2;
+            total_line_combos += x.second * (x.second - 1) / 2; // n choose 2
             total_line_combos %= MOD;
         }
 
         long long ans = 0;
         for (auto& x: y_cnt) {
             total_line_combos -= x.second * (x.second - 1) / 2; // remove the current y from the set of possible values
-            total_line_combos %= MOD;
-            total_line_combos += MOD;
-            total_line_combos %= MOD;
+            total_line_combos = ((total_line_combos % MOD) + MOD) % MOD;
             ans += (x.second * (x.second - 1) / 2) * total_line_combos;
             ans %= MOD;
         }
