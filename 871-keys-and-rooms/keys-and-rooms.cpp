ccf60eaf4efc50352dbc1ef8b1820a7397a81c0c
Steven Gong <gong.steven@hotmail.com>
2025-12-05 01:04:32 -0500

    Time: 0 ms (100.00%) | Memory: 14.4 MB (74.47%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -7,20 +7,21 @@ public:
         Maintain a visited list.
         Iterate over rooms, push to a queue / stack and keep popping the stsack
         */
-        unordered_set<int> visited;
+        vector<bool> visited(rooms.size(), false);;
         stack<int> s;
         s.push(0);
-        visited.insert(0);
+        visited[0] = true;
         while (!s.empty()) {
             int room = s.top();s.pop();
             for (auto key: rooms[room]) {
-                if (visited.count(key)) continue;
-                visited.insert(key);
+                if (visited[key]) continue;
+                visited[key] = true;
                 s.push(key);
             }
         }
-        return visited.size() == rooms.size();
-        
+        bool works = true;
+        for (int i = 0;i<visited.size();i++) works = works && visited[i];
+        return works;
         
     }
 };
