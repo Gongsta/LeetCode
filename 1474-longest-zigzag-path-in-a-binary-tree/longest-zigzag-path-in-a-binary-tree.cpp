b283136ff774f4e1b556cd3d100cb1f1aad42e38
Steven Gong <gong.steven@hotmail.com>
2025-12-04 23:03:38 -0500

    Time: 0 ms (100.00%) | Memory: 95.1 MB (64.37%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -11,18 +11,19 @@
  */
 class Solution {
 public:
-    map<pair<TreeNode*, char>, int> cnt;
-    int dfs(TreeNode* root, char d) {
-        if (!root) return -1;
-        if (cnt.count({root, d})) return cnt[{root,d}];
-        if (d == 'l') return cnt[{root, d}] = 1 + dfs(root->left, 'r');
-        return cnt[{root,d}] = 1 + dfs(root->right, 'l');
+    int ans = 0;
+    pair<int, int> dfs(TreeNode* root) {
+        if (!root) return {-1, -1};
+        auto [ll, lr] = dfs(root->left);
+        auto [rl, rr] = dfs(root->right);
+
+        ans = max(ans, max(lr + 1, rl + 1));
+        return {lr + 1, rl + 1};
     }
 
     int longestZigZag(TreeNode* root) {
-        if (!root) return 0;
-        int others = max(longestZigZag(root->left), longestZigZag(root->right));
-        return max(max(dfs(root, 'l'), dfs(root, 'r')), others);
+        dfs(root);
+        return ans;
         
     }
 };
