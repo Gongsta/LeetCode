4ec9afaacfedff0aef272aad885bd4ba642c167e
Steven Gong <gong.steven@hotmail.com>
2025-12-06 17:08:00 -0500

    Time: 11 ms (76.61%) | Memory: 19.7 MB (42.81%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.cpp
+++ 1272-invalid-transactions/invalid-transactions.cpp
@@ -1,62 +1,56 @@
 class Solution {
-public:
-    tuple<string, int, int, string, int> parse_string(string& str, int idx) {
-        stringstream ss{str};
-        string token;
+    struct Transaction {
         string name;
         int time;
         int amount;
         string location;
-        for (int i = 0;i<4;i++) {
-            getline(ss, token, ',');
-            if (i == 0) {
-                name = token;
-            } else if (i == 1) {
-                time = stoi(token);
-            } else if (i == 2) {
-                amount = stoi(token);
-            } else {
-                location = token;
-            }
-        }
-        return {name, time, amount, location, idx};
-
+        int idx;
+    };
+public:
+    Transaction parse_string(string& str, int idx) {
+        stringstream ss{str};
+        string name;
+        string time;
+        string amount;
+        string location;
+        getline(ss, name, ',');
+        getline(ss, time, ',');
+        getline(ss, amount, ',');
+        getline(ss, location, ',');
+        return Transaction{name, stoi(time), stoi(amount), location, idx};
     }
+
     vector<string> invalidTransactions(vector<string>& transactions) {
         /*
         Group by individual, and sort by time. So we can just sort the transactions
         There might be multiple transactions. It's enough to look at the last and next transaction.
         */
-        vector<tuple<string, int, int, string, int>> parsed_transactions;
+        vector<Transaction> parsed_transactions;
         for (int i = 0;i<transactions.size();i++) {
             parsed_transactions.push_back(parse_string(transactions[i],i));
         }
-        sort(parsed_transactions.begin(), parsed_transactions.end());
-        vector<bool> is_valid(transactions.size(), true);
+        sort(parsed_transactions.begin(), parsed_transactions.end(), [](const Transaction& a, const Transaction& b) {
+        if (a.name != b.name) return a.name < b.name;
+        return a.time < b.time;});
+        vector<bool> is_invalid(transactions.size(), false);
+
         for (int i = 0;i<parsed_transactions.size();i++) {
             // Check for amount
-            auto [name, time, amount, location, _] = parsed_transactions[i];
-            if (amount > 1000) {
-                is_valid[i] = false;
-            }
-            // Check previous
-            int j = i -1;
-            while (j >= 0) {
-                auto [prev_name, prev_time, prev_amount, prev_location, _] = parsed_transactions[j];
+            auto [name, time, amount, location, idx] = parsed_transactions[i];
+            if (amount > 1000) is_invalid[idx] = true;
+            // Check previous values
+            for (int j = i-1;j >= 0;j--) {
+                auto [prev_name, prev_time, prev_amount, prev_location, prev_idx] = parsed_transactions[j];
                 if (prev_name != name || time - prev_time > 60) break;
                 if (prev_location != location)  {
-                    is_valid[i] = false;
-                    is_valid[j] = false;
+                    is_invalid[idx] = true;
+                    is_invalid[prev_idx] = true;
                 }
-                j--;
             }
         }
         vector<string> ans;
-        for (int i = 0;i<is_valid.size();i++) {
-            if (!is_valid[i]) {
-                auto [_, _, _, _, idx] = parsed_transactions[i];
-                ans.push_back(transactions[idx]);
-            }
+        for (int i = 0;i<is_invalid.size();i++) {
+            if (is_invalid[i]) ans.push_back(transactions[i]);
         }
         return ans;
     }
