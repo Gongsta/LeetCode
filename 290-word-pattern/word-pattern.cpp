389706939e8a0524d94c81fc63fb04ff6659a53e
Steven Gong <gong.steven@hotmail.com>
2025-12-06 16:57:48 -0500

    Time: 0 ms (100.00%) | Memory: 8.5 MB (98.52%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -4,16 +4,16 @@ public:
         string char_to_word[26];
         stringstream ss{s};
         string word;
-        for (int i = 0;i<pattern.size();i++) {
-            ss >> word;
-            int c = pattern[i] - 'a';
-            if (char_to_word[c].empty()) {
-                // We will map this character to the word, but first check that the word is not there
-                for (int i = 0;i<26;i++) {
-                    if (char_to_word[i] == word) return false;
+
+        for (char c: pattern) {
+            if (!(ss >> word)) return false;
+            int c_idx = c - 'a';
+            if (char_to_word[c_idx].empty()) {
+                for (int j = 0;j<26;j++) {
+                    if (char_to_word[j] == word) return false;
                 }
-                char_to_word[c] = word;
-            } else if (char_to_word[c] != word) {
+                char_to_word[c_idx] = word;
+            } else if (char_to_word[c_idx] != word) {
                 return false;
             }
         }
