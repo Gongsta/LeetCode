b436c0e625839611a073d19caf004dc37bef892d
Steven Gong <gong.steven@hotmail.com>
2025-12-10 20:27:14 -0500

    Time: 95 ms (5.74%) | Memory: 119.7 MB (5.26%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -2,16 +2,18 @@ class Solution {
     int MOD = (int)1e9 + 7;
 public:
 
-    long long factorial(long long n) {
-        if (n == 1) return 1;
-        return (n * factorial(n-1)) % MOD;
-    }
     int countPermutations(vector<int>& complexity) {
         map<int, int> freq;
         for (const int& c: complexity) freq[c]++;
-        auto [min_num, cnt] = *(freq.begin());
+        auto& [min_num, cnt] = *(freq.begin());
         if (cnt > 1) return 0;
         if (complexity[0] != min_num) return 0;
-        return factorial(complexity.size() - 1);
+
+        long long ans = 1;
+        for (int i = 2;i<=complexity.size() - 1;i++) {
+            ans *= i;
+            ans %= MOD;
+        }
+        return ans;
     }
 };
