fc5d36e1d7615606207c03323c8e693752375172
Steven Gong <gong.steven@hotmail.com>
2025-12-15 18:27:31 -0500

    Time: 0 ms (100.00%) | Memory: 15.3 MB (92.88%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int dp[n+1][m+1];
+        memset(dp, 0, sizeof(dp));
+
+        for (int i = 1;i<=n;i++) {
+            for (int j = 1;j<=m;j++) {
+                if (i == 1) {
+                    dp[i][j] = dp[i][j-1] + grid[i-1][j-1];
+                } else if (j == 1) {
+                    dp[i][j] = dp[i-1][j] + grid[i-1][j-1];
+                } else {
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i-1][j-1]; // dp[i][j] = mininum path sum passing through (i-1, j-1)
+                }
+            }
+        }
+        return dp[n][m];
+        
+    }
+};
