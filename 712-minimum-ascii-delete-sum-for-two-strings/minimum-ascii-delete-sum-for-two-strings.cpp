59e947c6c35b9f594b03ad186589ecba1cfbe3c9
Steven Gong <gong.steven@hotmail.com>
2026-01-11 04:38:30 +0800

    Time: 33 ms (30.35%) | Memory: 18.5 MB (9.58%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,22 +1,23 @@
 class Solution {
-    map<pair<int, int>, int> m;
 public:
-    int minimumDeleteSumInner(const string& s1, const string& s2, int i, int j) {
-        if (m.count({i,j})) return m[{i,j}];
+    int minimumDeleteSumInner(const string& s1, const string& s2, int i, int j, vector<vector<int>>& m) {
+        if (m[i][j] != INT_MIN) return m[i][j];
         if (i == s1.size()) {
             int ans = 0;
             for (int k = j;k<s2.size();k++) ans += s2[k];
-            return m[{i,j}] = ans;
+            return m[i][j] = ans;
         }
         if (j == s2.size()) {
             int ans = 0;
             for (int k = i;k<s1.size();k++) ans += s1[k];
-            return m[{i,j}] = ans;
+            return m[i][j] = ans;
         }
-        if (s1[i] == s2[j]) return m[{i,j}] = minimumDeleteSumInner(s1, s2, i+1, j+1);
-        return m[{i,j}] = min(minimumDeleteSumInner(s1, s2, i + 1, j) + (int)s1[i], minimumDeleteSumInner(s1, s2, i, j+1) + (int)s2[j]);
+        if (s1[i] == s2[j]) return m[i][j] = minimumDeleteSumInner(s1, s2, i+1, j+1, m);
+        return m[i][j] = min(minimumDeleteSumInner(s1, s2, i + 1, j, m) + (int)s1[i], minimumDeleteSumInner(s1, s2, i, j+1, m) + (int)s2[j]);
     }
     int minimumDeleteSum(string s1, string s2) {
-        return minimumDeleteSumInner(s1, s2, 0, 0);
+
+        vector<vector<int>> m(s1.size() + 1, vector<int>(s2.size() + 1, INT_MIN));
+        return minimumDeleteSumInner(s1, s2, 0, 0, m);
     }
 };
