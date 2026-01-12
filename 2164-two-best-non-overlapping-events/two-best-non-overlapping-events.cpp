e092881d9fdf3e9d81aeacede3f0e666018995a6
Steven Gong <gong.steven@hotmail.com>
2026-01-13 03:24:20 +0800

    Time: 104 ms (41.22%) | Memory: 158.6 MB (73.06%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -9,10 +9,11 @@ public:
             suffix[i] = max(events[i][2], suffix[i + 1]);
         }
         int ans = events[0][2];
+        vector<int> v = {INT_MAX, INT_MAX, INT_MAX};
         for (int i = 0;i<n;i++) {
             ans = max(ans, events[i][2]);
             // binary search start greater than the current end
-            vector<int> v = {events[i][1], INT_MAX, INT_MAX};
+            v[0] = events[i][1];
             auto it = upper_bound(events.begin(), events.end(), v);
             if (it != events.end()) {
                 int idx = it - events.begin();
