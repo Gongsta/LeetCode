a3fbd5b1759778d681ba0e1ad3467d62b0c80c63
Steven Gong <gong.steven@hotmail.com>
2026-01-17 18:30:53 -0500

    Time: 15 ms (42.92%) | Memory: 20.7 MB (33.49%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int numSubmat(vector<vector<int>>& mat) {
-        int ans = 0;
         int n = mat.size();
         int m = mat[0].size();
+        int ans = 0;
+        vector<int> hist(m, 0);
         for (int i = 0;i<n;i++) {
             for (int j = 0;j<m;j++) {
-                int width = INT_MAX;
-                for (int k = i;k<n;k++) {
-                    int curr_width = 0;
-                    for (int l = j;l<m;l++) {
-                        if (mat[k][l] != 1) break;
-                        curr_width++;
-                    }
-                    width = min(width, curr_width);
-                    if (width == 0) break;
-                    ans += width;
+                if (mat[i][j] == 1) {
+                    hist[j]++;
+                } else {
+                    hist[j] = 0;
                 }
+            }
+            stack<pair<int, int>> st; // {height, count}
+            int sum = 0;
+            for (int j = 0;j<m;j++) {
+                int count = 1;
+                int height = hist[j];
+                while (!st.empty() && height <= st.top().first)  {
+                    sum -= (st.top().first * st.top().second);
+                    count += st.top().second;
+                    st.pop();
+                }
+                sum += height * count;
+                st.push({height, count});
+                ans += sum;
             }
         }
 
