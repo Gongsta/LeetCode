27c64ed968807b2ecb7a0700e1e0b7243d316f2a
Steven Gong <gong.steven@hotmail.com>
2026-01-18 17:03:30 -0500

    Time: 144 ms (16.67%) | Memory: 337.4 MB (8.33%) - LeetSync

--- 4067-design-auction-system/design-auction-system.cpp
+++ 4067-design-auction-system/design-auction-system.cpp
@@ -1,43 +1,38 @@
 class AuctionSystem {
-    map<pair<int,int>, int> useritem_to_amount;
-    unordered_map<int, priority_queue<pair<int, int>>> item_to_user; 
+    using Key = pair<int,int>; // (userId, itemId)
+
+    map<Key, int> useritem_to_amount;
+    map<int, priority_queue<pair<int,int>>> item_to_user; // (amount, userId)
+
 public:
-    AuctionSystem() {
-        }
-    
     void addBid(int userId, int itemId, int bidAmount) {
         useritem_to_amount[{userId, itemId}] = bidAmount;
         item_to_user[itemId].push({bidAmount, userId});
     }
-    
+
     void updateBid(int userId, int itemId, int newAmount) {
         useritem_to_amount[{userId, itemId}] = newAmount;
         item_to_user[itemId].push({newAmount, userId});
     }
-    
+
     void removeBid(int userId, int itemId) {
         useritem_to_amount.erase({userId, itemId});
     }
-    
+
     int getHighestBidder(int itemId) {
-        int highest_user = -1;
-        while (!item_to_user[itemId].empty()) {
-            auto [amnt, user] = item_to_user[itemId].top();
-            if (useritem_to_amount[{user, itemId}] == amnt) {
-                highest_user = user;
-                break;
+        auto itItem = item_to_user.find(itemId);
+        if (itItem == item_to_user.end()) return -1;
+
+        auto &pq = itItem->second;
+        while (!pq.empty()) {
+            auto [amt, user] = pq.top();
+
+            auto it = useritem_to_amount.find({user, itemId});
+            if (it != useritem_to_amount.end() && it->second == amt) {
+                return user; // valid top
             }
-            item_to_user[itemId].pop();
+            pq.pop(); // stale
         }
-        return highest_user;
+        return -1;
     }
 };
-
-/**
- * Your AuctionSystem object will be instantiated and called as such:
- * AuctionSystem* obj = new AuctionSystem();
- * obj->addBid(userId,itemId,bidAmount);
- * obj->updateBid(userId,itemId,newAmount);
- * obj->removeBid(userId,itemId);
- * int param_4 = obj->getHighestBidder(itemId);
- */
