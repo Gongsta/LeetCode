8f932834295546d9fc1277f610fb028eb416df1c
Steven Gong <gong.steven@hotmail.com>
2026-01-18 17:15:17 -0500

    Time: 165 ms (8.33%) | Memory: 362.1 MB (8.33%) - LeetSync

--- 4067-design-auction-system/design-auction-system.cpp
+++ 4067-design-auction-system/design-auction-system.cpp
@@ -1,43 +1,39 @@
+// Dan4Life
+// O(log n) per operation
+#include <bits/stdc++.h>
+using namespace std;
+
 class AuctionSystem {
-    unordered_map<int, unordered_map<int,int>> useritem_to_amount;
-    unordered_map<int, set<pair<int, int>, greater<pair<int, int>>>> item_to_user;
+    unordered_map<int, unordered_map<int,int>> mp;
+    unordered_map<int, set<pair<long long,int>>> st;
+
 public:
-    AuctionSystem() {
-    }
-    
+    AuctionSystem() {}
+
     void addBid(int userId, int itemId, int bidAmount) {
-        if (useritem_to_amount[itemId].count(userId)) {
-            updateBid(userId, itemId, bidAmount);
-        } else {
-            useritem_to_amount[itemId][userId] = bidAmount;
-            item_to_user[itemId].insert({bidAmount, userId});
+        if (mp[itemId].count(userId)) {
+            long long old = mp[itemId][userId];
+            st[itemId].erase({old, userId});
         }
+        mp[itemId][userId] = bidAmount;
+        st[itemId].insert({bidAmount, userId});
     }
-    
+
     void updateBid(int userId, int itemId, int newAmount) {
-        int bidAmount = useritem_to_amount[itemId][userId];
-        useritem_to_amount[itemId][userId] = newAmount;
-        item_to_user[itemId].erase({bidAmount, userId});
-        item_to_user[itemId].insert({newAmount, userId});
+        long long old = mp[itemId][userId];
+        st[itemId].erase({old, userId});
+        mp[itemId][userId] = newAmount;
+        st[itemId].insert({newAmount, userId});
     }
-    
+
     void removeBid(int userId, int itemId) {
-        int amnt = useritem_to_amount[itemId][userId];
-        useritem_to_amount[itemId].erase(userId);
-        item_to_user[itemId].erase({amnt, userId});
+        long long old = mp[itemId][userId];
+        st[itemId].erase({old, userId});
+        mp[itemId].erase(userId);
     }
-    
+
     int getHighestBidder(int itemId) {
-        if (item_to_user[itemId].size() == 0) return -1;
-        return item_to_user[itemId].begin()->second;
+        if (!st.count(itemId) || st[itemId].empty()) return -1;
+        return prev(st[itemId].end())->second;
     }
 };
-
-/**
- * Your AuctionSystem object will be instantiated and called as such:
- * AuctionSystem* obj = new AuctionSystem();
- * obj->addBid(userId,itemId,bidAmount);
- * obj->updateBid(userId,itemId,newAmount);
- * obj->removeBid(userId,itemId);
- * int param_4 = obj->getHighestBidder(itemId);
- */
