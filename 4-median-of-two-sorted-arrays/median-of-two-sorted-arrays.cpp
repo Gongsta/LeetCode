47d5fd3e5e98a455100df0893ddbfd1f9c00a526
Steven Gong <gong.steven@hotmail.com>
2026-01-17 19:04:55 -0500

    Time: 0 ms (100.00%) | Memory: 95 MB (93.59%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,36 +1,31 @@
 class Solution {
-    priority_queue<int> left_pq;
-    priority_queue<int, vector<int>, greater<int>> right_pq;
 public:
-    void insert(int num) {
-        if (left_pq.empty() || num <= left_pq.top()) {
-            left_pq.push(num);
-        } else {
-            right_pq.push(num);
-        }
-
-        if (left_pq.size() > right_pq.size() + 1) {
-            right_pq.push(left_pq.top());
-            left_pq.pop();
-        } else if (right_pq.size() > left_pq.size()) {
-            left_pq.push(right_pq.top());
-            right_pq.pop();
-        }
-    }
-    
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        for (int num: nums1) {
-            insert(num);
-        }
-        for (int num: nums2) {
-            insert(num);
-        }
-        if (left_pq.size() == right_pq.size()) {
-            long long left = left_pq.top(); 
-            long long right = right_pq.top();
-            return (right + left) / 2.0;
-        }
-        return left_pq.top();
+        if (nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
 
+        int total = (nums1.size() + nums2.size());
+        int half = (total + 1) / 2;
+
+        int l = 0;
+        int r = min((int)nums1.size(), half);
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int mid2 = half - mid;
+            int l1 = mid == 0 ?  INT_MIN : nums1[mid - 1];
+            int r1 = mid == nums1.size() ? INT_MAX : nums1[mid];
+            int l2 = mid2 == 0 ?  INT_MIN : nums2[mid2 - 1];
+            int r2 = mid2 == nums2.size() ? INT_MAX : nums2[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (total % 2 == 1) return max(l1, l2);
+                return ((double)(min(r1, r2)) + (double)(max(l1, l2))) / 2.0;
+            } else if (l1 > r2) {
+                r = mid -1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return 0.0;
     }
 };
