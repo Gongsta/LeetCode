72ca404be0f1c2d4b4ecfda8db30984b4a4cef28
Steven Gong <gong.steven@hotmail.com>
2026-01-18 23:28:38 -0500

    Time: 65 ms (22.27%) | Memory: 35.5 MB (5.45%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -12,22 +12,31 @@ public:
                     prefix_row[row][col] += prefix_row[row][col-1];
                 }
                 if (row > 0) {
-                    prefix_col[row][col] += prefix_row[row-1][col];
+                    prefix_col[row][col] += prefix_col[row -1][col];
+
                 }
             }
         }
 
         for (int row = 0;row<n;row++) {
             for (int col = 0;col < m;col++) {
+                int curr_sum = 0;
                 for (int width = 1;width<=min(n - row, m - col);width++) {
-                    int curr_sum = 0;
-                    for (int r = row;r < row + width;r++) {
-                            if (col == 0) {
-                                curr_sum += prefix_row[r][col + width - 1];
-                            } else {
-                                curr_sum += prefix_row[r][col + width - 1] - prefix_row[r][col - 1];
-                            }
-                        }
+                    int new_row = row + width - 1;
+                    int new_col = col + width - 1;
+                    if (col == 0) {
+                        curr_sum += prefix_row[new_row][new_col];
+                    } else {
+                        curr_sum += prefix_row[new_row][new_col] - prefix_row[new_row][col - 1];
+                    }
+
+                    if (row == 0) {
+                        curr_sum += prefix_col[new_row][new_col];
+                    } else {
+                        curr_sum += prefix_col[new_row][new_col] - prefix_col[row - 1][new_col];
+                    }
+                    curr_sum -= mat[new_row][new_col]; // double counted
+
                     if (curr_sum <= threshold) {
                         ans = max(ans, width);
                     } else {
